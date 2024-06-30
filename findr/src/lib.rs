6273e07c87ab2402b29ac7af257f52012dffd2e7
tky <takayuki.for.public@gmail.com>
2024-06-30 19:56:11 +0900

    Refactor

--- findr/src/lib.rs
+++ findr/src/lib.rs
@@ -1,6 +1,6 @@
 use clap::{ArgAction, Parser, ValueEnum};
 use regex::Regex;
-use walkdir::WalkDir;
+use walkdir::{WalkDir, DirEntry};
 use std::error::Error;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -54,29 +54,32 @@ pub fn get_args() -> MyResult<Config> {
 
 pub fn run(config: Config) -> MyResult<()> {
     for path in config.paths {
-        for entry in WalkDir::new(path) {
-            match entry {
-                Err(e) => eprintln!("{}", e),
-                Ok(entry) => {
-                    if should_display(&entry, &config.names, &config.entry_types) {
-                        println!("{}", entry.path().display())
-                    }
-                },
-            }
-        }
+        let name_filter = |entry: &DirEntry| -> bool {
+            config.names.is_empty() || config.names.iter().any(|re| re.is_match(&entry.file_name().to_string_lossy()))
+        };
+
+        let type_filter = |entry: &DirEntry| -> bool {
+            config.entry_types.is_empty() || config.entry_types.iter().any(|entry_type| {
+                match entry_type {
+                    EntryType::Link => entry.file_type().is_symlink(),
+                    EntryType::File => entry.file_type().is_file(),
+                    EntryType::Dir => entry.file_type().is_dir(),
+                }
+            })
+        };
+
+        WalkDir::new(path)
+            .into_iter()
+            .filter_map(|e| match e {
+                Err(e)=> {
+                    eprintln!("{}", e);
+                    None
+                }
+                Ok(entry) => Some(entry),
+            }).filter(type_filter)
+        .filter(name_filter)
+        .map(|entry| entry.path().display().to_string())
+        .for_each(|entry| println!("{}", entry));
     }
     Ok(())
-}
-
-
-fn should_display(entry: &walkdir::DirEntry, names: &Vec<Regex>, entry_types: &Vec<EntryType>) -> bool {
-    (names.is_empty() || names.iter().any(|re| re.is_match(&entry.file_name().to_string_lossy())))
-        &&
-    (entry_types.is_empty() || entry_types.iter().any(|entry_type| {
-        match entry_type {
-            EntryType::Link => entry.file_type().is_symlink(),
-            EntryType::File => entry.file_type().is_file(),
-            EntryType::Dir => entry.file_type().is_dir(),
-        }
-    }))
 }
