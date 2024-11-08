17c8422bc25812c4135843f07c9c552bcf76a1d1
tky <takayuki.for.public@gmail.com>
2024-11-08 20:14:55 +0900

    Add unit test and enum

--- lsr/src/main.rs
+++ lsr/src/main.rs
@@ -1,5 +1,9 @@
+mod owner;
+
 use clap::Parser;
 use anyhow::{anyhow, Result};
+use std::path::PathBuf;
+use owner::Owner;
 
 
 #[derive(Parser, Debug)]
@@ -30,4 +34,183 @@ fn main() {
 fn run(args: Args) -> Result<()> {
     println!("{:?}", args);
     Ok(())
+}
+
+fn find_files(
+    paths: &[String],
+    show_hidden: bool,
+    ) -> Result<Vec<PathBuf>> {
+    unimplemented!()
+}
+
+fn format_mode(mode: u32) -> String {
+    unimplemented!()
+}
+
+fn format_output(paths: &[PathBuf]) -> Result<String> {
+    unimplemented!()
+}
+
+fn mk_triple(mode: u32, owner: Owner) -> String {
+}
+
+// --------------------------------------------------
+#[cfg(test)]
+mod test {
+    use super::{find_files, format_mode, format_output, mk_triple, Owner};
+    use pretty_assertions::assert_eq;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_find_files() {
+        // Find all non-hidden entries in a directory
+        let res = find_files(&["tests/inputs".to_string()], false);
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            [
+                "tests/inputs/bustle.txt",
+                "tests/inputs/dir",
+                "tests/inputs/empty.txt",
+                "tests/inputs/fox.txt",
+            ]
+        );
+
+        // Any existing file should be found even if hidden
+        let res = find_files(&["tests/inputs/.hidden".to_string()], false);
+        assert!(res.is_ok());
+        let filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        assert_eq!(filenames, ["tests/inputs/.hidden"]);
+
+        // Test multiple path arguments
+        let res = find_files(
+            &[
+                "tests/inputs/bustle.txt".to_string(),
+                "tests/inputs/dir".to_string(),
+            ],
+            false,
+        );
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            ["tests/inputs/bustle.txt", "tests/inputs/dir/spiders.txt"]
+        );
+    }
+
+    #[test]
+    fn test_find_files_hidden() {
+        // Find all entries in a directory including hidden
+        let res = find_files(&["tests/inputs".to_string()], true);
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            [
+                "tests/inputs/.hidden",
+                "tests/inputs/bustle.txt",
+                "tests/inputs/dir",
+                "tests/inputs/empty.txt",
+                "tests/inputs/fox.txt",
+            ]
+        );
+    }
+
+    fn long_match(
+        line: &str,
+        expected_name: &str,
+        expected_perms: &str,
+        expected_size: Option<&str>,
+    ) {
+        let parts: Vec<_> = line.split_whitespace().collect();
+        assert!(!parts.is_empty() && parts.len() <= 10);
+
+        let perms = parts.first().unwrap();
+        assert_eq!(perms, &expected_perms);
+
+        if let Some(size) = expected_size {
+            let file_size = parts.get(4).unwrap();
+            assert_eq!(file_size, &size);
+        }
+
+        let display_name = parts.last().unwrap();
+        assert_eq!(display_name, &expected_name);
+    }
+
+    #[test]
+    fn test_format_output_one() {
+        let bustle_path = "tests/inputs/bustle.txt";
+        let bustle = PathBuf::from(bustle_path);
+
+        let res = format_output(&[bustle]);
+        assert!(res.is_ok());
+
+        let out = res.unwrap();
+        let lines: Vec<&str> =
+            out.split('\n').filter(|s| !s.is_empty()).collect();
+        assert_eq!(lines.len(), 1);
+
+        let line1 = lines.first().unwrap();
+        long_match(line1, bustle_path, "-rw-r--r--", Some("193"));
+    }
+
+    #[test]
+    fn test_format_output_two() {
+        let res = format_output(&[
+            PathBuf::from("tests/inputs/dir"),
+            PathBuf::from("tests/inputs/empty.txt"),
+        ]);
+        assert!(res.is_ok());
+
+        let out = res.unwrap();
+        let mut lines: Vec<&str> =
+            out.split('\n').filter(|s| !s.is_empty()).collect();
+        lines.sort();
+        assert_eq!(lines.len(), 2);
+
+        let empty_line = lines.remove(0);
+        long_match(
+            empty_line,
+            "tests/inputs/empty.txt",
+            "-rw-r--r--",
+            Some("0"),
+        );
+
+        let dir_line = lines.remove(0);
+        long_match(dir_line, "tests/inputs/dir", "drwxr-xr-x", None);
+    }
+
+    #[test]
+    fn test_mk_triple() {
+        assert_eq!(mk_triple(0o751, Owner::User), "rwx");
+        assert_eq!(mk_triple(0o751, Owner::Group), "r-x");
+        assert_eq!(mk_triple(0o751, Owner::Other), "--x");
+        assert_eq!(mk_triple(0o600, Owner::Other), "---");
+    }
+
+    #[test]
+    fn test_format_mode() {
+        assert_eq!(format_mode(0o755), "rwxr-xr-x");
+        assert_eq!(format_mode(0o421), "r---w---x");
+    }
 }
